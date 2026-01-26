e72e47e1f91c75ecc5e80bcaba6ba6e9a8472488
Anurag rawat <arwt8947@gmail.com>
2026-01-26 18:48:38 +0530

    chore: update project config for repo rename to MyFolio

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import path from "path"
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/React-portFolio/',
+  base: '/MyFolio/',
   plugins: [react()],
   resolve: {
     alias: {
